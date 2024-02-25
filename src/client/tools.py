32c718a56046ce148b8f1c71499e98336ac1840d
justkingyt1 <clashroyalevadim8@gmail.com>
2024-02-25 19:42:58 +0300

    music filling now takes place in a separate thread

--- src/client/tools.py
+++ src/client/tools.py
@@ -1,20 +1,6 @@
 from PySide6 import QtGui, QtWidgets, QtCore
 import settings
 
-
-class WorkerThread(QtCore.QThread):
-    finished = QtCore.Signal()
-
-    def __init__(self, func, *args, **kwargs) -> None:
-        super(WorkerThread, self).__init__()
-        self.func = func
-        self.args = args
-        self.kwargs = kwargs
-    
-    def run(self) -> None:
-        self.func(*self.args, **self.kwargs)
-        self.finished.emit()
-
 def get_pixmap(name: str) -> None:
     return QtGui.QPixmap(f'{settings.IMG_DIR}/{name}.png')
 
